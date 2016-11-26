e2bb01504ea2ba630da7af65bdbfc0c00dd25bca
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-11-26 20:53:39 +0200

    refactor: reworked SlidingBlocks::State::Move so that it creates a shared pointer to the moved state in the very beginning

--- source/State.cpp
+++ source/State.cpp
@@ -27,16 +27,16 @@ bool SlidingBlocks::State::operator==(const SlidingBlocks::State& other) const
 std::shared_ptr<SlidingBlocks::State> SlidingBlocks::State::Move(Step movement) const
 {
     auto canMove = false;
-    auto moved = *this;
-    moved.movement = movement;
+    std::shared_ptr<State> moved = std::make_shared<SlidingBlocks::State>(*this);
+    moved->movement = movement;
 
     switch (movement)
     {
         case Step::Left:
             if (emptyCellX < Size() - 1)
             {
-                moved[moved.emptyCellY][moved.emptyCellX] = moved[moved.emptyCellY][moved.emptyCellX + 1];
-                moved.emptyCellX++;
+                (*moved)[moved->emptyCellY][moved->emptyCellX] = (*moved)[moved->emptyCellY][moved->emptyCellX + 1];
+                moved->emptyCellX++;
                 canMove = true;
             }
             break;
@@ -44,8 +44,8 @@ std::shared_ptr<SlidingBlocks::State> SlidingBlocks::State::Move(Step movement) 
         case Step::Right:
             if (emptyCellX > 0)
             {
-                moved[moved.emptyCellY][moved.emptyCellX] = moved[moved.emptyCellY][moved.emptyCellX - 1];
-                moved.emptyCellX--;
+                (*moved)[moved->emptyCellY][moved->emptyCellX] = (*moved)[moved->emptyCellY][moved->emptyCellX - 1];
+                moved->emptyCellX--;
                 canMove = true;
             }
             break;
@@ -53,8 +53,8 @@ std::shared_ptr<SlidingBlocks::State> SlidingBlocks::State::Move(Step movement) 
         case Step::Down:
             if (emptyCellY < Size() - 1)
             {
-                moved[moved.emptyCellY][moved.emptyCellX] = moved[moved.emptyCellY + 1][moved.emptyCellX];
-                moved.emptyCellX++;
+                (*moved)[moved->emptyCellY][moved->emptyCellX] = (*moved)[moved->emptyCellY + 1][moved->emptyCellX];
+                moved->emptyCellX++;
                 canMove = true;
             }
             break;
@@ -62,16 +62,16 @@ std::shared_ptr<SlidingBlocks::State> SlidingBlocks::State::Move(Step movement) 
         case Step::Up:
             if (emptyCellY > 0)
             {
-                moved[moved.emptyCellY][moved.emptyCellX] = moved[moved.emptyCellY - 1][moved.emptyCellX];
-                moved.emptyCellX--;
+                (*moved)[moved->emptyCellY][moved->emptyCellX] = (*moved)[moved->emptyCellY - 1][moved->emptyCellX];
+                moved->emptyCellX--;
                 canMove = true;
             }
             break;
     }
 
     if (canMove)
-        moved[emptyCellY][emptyCellX] = 0;
-    return canMove ? std::make_shared<SlidingBlocks::State>(moved) : nullptr;
+        (*moved)[emptyCellY][emptyCellX] = 0;
+    return canMove ? moved : nullptr;
 }
 
 void SlidingBlocks::State::Print() const
